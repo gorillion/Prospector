5553b2bff2893e12f390e84bd8ae5701c641df9a
David Farrell <gorillion@users.noreply.github.com>
2019-01-13 10:04:04 -0500

    layers + other progress

--- Assets/__Scripts/CardProspector.cs
+++ Assets/__Scripts/CardProspector.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardProspector : MonoBehaviour {
-    
+public enum eCardState {
+	drawpile,
+	tableau,
+	target,
+	discard
+}
+
+public class CardProspector : Card {
+	[Header ("Set Dynamically : CardProspector")]
+	public eCardState state = eCardState.drawpile;
+	public List<CardProspector> hiddenBy = new List<CardProspector> ();
+	public int layoutID;
+	public SlotDef slotDef;
+
     void Start() {
         
     }
