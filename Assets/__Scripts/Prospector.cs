27824e1d293158ae877f42815173d607c9ce73a2
David Farrell <gorillion@users.noreply.github.com>
2019-01-14 18:33:32 -0500

    finished project
    weird blue bar at the top but it works otherwise

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -17,6 +17,8 @@ public class Prospector : MonoBehaviour {
 	public Vector2 fsPosRun = new Vector2(0.5f, 0.75f);
 	public Vector2 fsPosMid2 = new Vector2(0.4f, 1.0f);
 	public Vector2 fsPosEnd = new Vector2(0.5f, 0.95f);
+	public float reloadDelay = 5f;
+	public Text gameOverText, roundResultText, highScoreText;
 
 	[Header ("Set Dynamically")]
 	public Deck deck;
@@ -30,6 +32,32 @@ public class Prospector : MonoBehaviour {
 
 	void Awake () {
 		S = this;
+		SetUpUITexts();
+	}
+
+	void SetUpUITexts () {
+		GameObject go = GameObject.Find("HighScore");
+		if (go != null) {
+			highScoreText = go.GetComponent<Text>();
+		}
+		int highScore = ScoreManager.HIGH_SCORE;
+		string hScore = "High Score: " + Utils.AddCommasToNumber(highScore);
+		go.GetComponent<Text>().text = hScore;
+		go = GameObject.Find("GameOver");
+		if(go != null) {
+			gameOverText = go.GetComponent<Text>();
+		}
+		go = GameObject.Find("RoundResult");
+		if(go != null) {
+			roundResultText = go.GetComponent<Text>();
+		}
+
+		ShowResultsUI(false);
+	}
+
+	void ShowResultsUI(bool show) {
+		gameOverText.gameObject.SetActive(show);
+		roundResultText.gameObject.SetActive(show);
 	}
 
     void Start() {
@@ -237,16 +265,31 @@ public class Prospector : MonoBehaviour {
 	}
 
 	void GameOver (bool won) {
+		int score = ScoreManager.SCORE;
+		if (fsRun != null) score += fsRun.score;
 		if (won) {
-			print("u win");
+			gameOverText.text = "Round Over";
+			roundResultText.text = "You Won This Round... \nRound Score: " + score;
+			ShowResultsUI(true);
 			ScoreManager.EVENT(eScoreEvent.gameWin);
 			FloatingScoreHandler(eScoreEvent.gameWin);
 		} else {
-			print("u lose");
+			gameOverText.text = "Game Over";
+			if(ScoreManager.HIGH_SCORE<= score) {
+				string str = "you got the high score! \n high score: " + score;
+				roundResultText.text = str;
+			} else {
+				roundResultText.text = "your final score was : " + score;
+			}
+			ShowResultsUI(true);
 			ScoreManager.EVENT(eScoreEvent.gameLoss);
 			FloatingScoreHandler(eScoreEvent.gameLoss);
 		}
 
+		Invoke("ReloadLevel", reloadDelay);
+	}
+
+	void ReloadLevel () {
 		SceneManager.LoadScene("__Prospector_Scene_0");
 	}
 
@@ -303,8 +346,4 @@ public class Prospector : MonoBehaviour {
 				break;
 		}
 	}
-
-	void Update() {
-        
-    }
 }
